6ad796b5ae10576797a3273edc82702ef09ae166
Tsering Lama <tseringlama@pursuit.org>
2020-01-23 17:06:07 -0500

    set up all the folders

---
+++ Persistance/FileManager.swift
@@ -0,0 +1,9 @@
+//
+//  FileManager.swift
+//  Pursuit-Core-iOS-PhotoJournal-Assignment
+//
+//  Created by Tsering Lama on 1/23/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import Foundation
