c14fee1255d0845c623ead53e364145708d8b898
Tsering Lama <tseringlama@pursuit.org>
2020-01-25 22:03:59 -0500

    set up UI

---
+++ View Controllers/AddingVC.swift
@@ -0,0 +1,18 @@
+//
+//  AddingVC.swift
+//  Pursuit-Core-iOS-PhotoJournal-Assignment
+//
+//  Created by Tsering Lama on 1/25/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+class AddingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+  
+}
