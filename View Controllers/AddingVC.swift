d852eea385a0d1b5433ceea2d22cd4a092a796e1
Tsering Lama <tseringlama@pursuit.org>
2020-01-27 09:38:03 -0500

    attempting to pass object from adding VC to the main VC

--- View Controllers/AddingVC.swift
+++ View Controllers/AddingVC.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
+protocol AddingDelegate: AnyObject {
+    func passInfo(vc: AddingVC)
+}
+
 class AddingVC: UIViewController {
     
     @IBOutlet weak var picDescription: UITextField!
     @IBOutlet weak var userPic: UIImageView!
     
     private var imagePicker = UIImagePickerController()
+    
+    weak var delegate: AddingDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,5 +41,12 @@ class AddingVC: UIViewController {
 }
 
 extension AddingVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        userPic.image = image
+        delegate?.passInfo(vc: self)
+        dismiss(animated: true)
+    }
 }
--- View Controllers/EntriesVC.swift
+++ View Controllers/EntriesVC.swift
@@ -19,7 +19,7 @@ class EntriesVC: UIViewController {
     
     private var selectImage: UIImage? {
         didSet {
-            
+            appendNewPhoto()
         }
     }
     
